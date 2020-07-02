bc5bc8ecc5ad611905b6e3a36fb2b2a305c31aac
James Rotering <james.rotering@supportuw.org>
2020-07-02 16:45:57 -0500

    Working validation on contact form, stopping point before I die

--- components/parts/Button.js
+++ components/parts/Button.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import styled from 'styled-components'
-import { colors,  sizes, breakpoints, mixins } from '../css-variables'
+import { mixins } from '../css-variables'
 
 const Button = ({ className, link, text, bgimage }) => {
     const classesList = bgimage ? `${className} ${className}--bgimage` : className
--- components/parts/TagList.js
+++ components/parts/TagList.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from 'styled-components'
-import { colors, mixins, sizes, breakpoints, fonts } from '../css-variables'
+import { colors, sizes, breakpoints } from '../css-variables'
 
 const TagList = ({ items, className }) => {
     const itemsList = items.map((item) => (
