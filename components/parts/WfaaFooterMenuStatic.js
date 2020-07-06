c98e574ac041b1273d778dc733ceb64925411abc
James Rotering <james.rotering@supportuw.org>
2020-07-06 14:50:40 -0500

    add static footer menu

---
+++ components/parts/WfaaFooterMenuStatic.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const FooterMenu = ({ menu, pageLink="#" }) => {
+
+  const { name, items } = menu
+
+      return (
+        <nav className="footer-menu">
+          <a href={pageLink}>
+            <div className="footer-menu__title" dangerouslySetInnerHTML={{ __html: name }} />
+          </a>
+          <ul>
+            {menu &&
+              items.map(
+                prop => (
+                  <li className="nav-item" key={prop.url}>
+                    <a
+                      className="nav-link active"
+                      href={prop.url}
+                      alt={prop.title}
+                    >
+                      {prop.title}
+                    </a>
+                  </li>
+                )
+              )}
+          </ul>
+        </nav>
+      )
+}
+
+export default FooterMenu
