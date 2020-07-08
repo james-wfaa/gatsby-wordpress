fd28f0afac015a96cc05033a64b37347303095f7
James Rotering <james.rotering@supportuw.org>
2020-07-08 11:27:24 -0500

    build out card example pages

--- src/pages/contentCardFlex.js
+++ src/pages/contentCardFlex.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../../components/Layout"
-import ContentCard from "../../components/content-blocks/ContentCard"
+import ContentCard from "../../components/content-blocks/ContentCardFlex"
 import PageSection from '../../components/page-sections/GenericPageSection'
 
 const taglist1 = [
@@ -74,7 +74,8 @@ export default ({ data }) => {
 return (
 <Layout>
     <PageSection pad>
-        <h1>Content Card</h1>
+        <h1>Content Card (FLEXIBLE HEIGHTS</h1>
+        <p><strong><em>Note: on this page, when there are references to layouts breaking down, it probably means that the card height is taller than the design due to excess content.</em></strong></p>
 
         <p>Content Cards can be for either stories (Story Card) or events (Event Card). They come in the following sizes:</p>
         <ul>
@@ -89,6 +90,7 @@ return (
         <p></p>
           <h2>1/3 (S) Content Card</h2>
           <p>This size card displays at 256px wide on mobile and 344px wide on all larger screens.</p>
+          <p>When an image is present, there is a 150 character limit on the excerpt.</p>
           
           <ContentCard 
         title="Coachella Valley"
@@ -98,7 +100,41 @@ return (
         img={data.cardImage1}
         tags={taglist2}
         />
-        <p>Event Card (S):</p>
+        <br />
+        <ContentCard 
+        title="Badger Bowl Firsts"
+        category="UW NOW"
+        excerpt="First played in 1902, the Rose Bowl is the nation’s oldest college football bowl game. This season, the Badgers will make their 10th trip to Pasadena for the big game and their first trip back since 2013.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere" 
+        url="##"
+        img={data.cardImage6}
+        tags={taglist2}
+        />
+        <br />
+        <p>Even with the 150 character exerpt limit, a long title can push the limits of the card height. The following example has a title that spans 4 lines on mobile, 
+          and the result is that the header section looks cramped.
+        </p>
+           <ContentCard 
+        title="The Past, Present, and Future of Rainstorms and Floods in Wisconsin and around the World"
+        category="UW NOW"
+        excerpt="First played in 1902, the Rose Bowl is the nation’s oldest college football bowl game. This season, the Badgers will make their 10th trip to Pasadena for the big game and their first trip back since 2013.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere" 
+        url="#"
+        tags={taglist2}
+        img={data.cardImage6}
+
+        />
+        <br />
+        <p>We probably will want to impose a character limit on titles as well in order to prevent this issue.</p>
+        <p>When there is no image, the excerpt limit is 250 characters.</p>
+        <ContentCard 
+        title="Coachella Valley"
+        category="UW NOW"
+        excerpt="La Quinta Resort and Club Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mollis vehicula hendrerit. Nullam sollicitudin tincidunt ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere. Nullam sollicitudin tincidunt ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere" 
+        url="#"
+        tags={taglist2}
+        />
+        <p></p>
+     
+        <p></p>
         <ContentCard 
         startDate="Apr. 3" 
         title="The Past, Present, and Future of Rainstorms and Floods in Wisconsin and around the World"
@@ -107,7 +143,18 @@ return (
         location="Madison" 
         img={data.cardImage3}
         />
-        <p>(S) size can be shown without an image.</p>
+        <br />
+        <p>If we add an end date, this card still looks okay at mobile despite the long title. But at tablet and desktop, the two-line date plus the long title cause a breakdown. </p>      
+        <ContentCard 
+        startDate="Apr. 3" 
+        endDate="Apr. 5"
+        title="The Past, Present, and Future of Rainstorms and Floods in Wisconsin and around the World"
+        category="Global Hot Spots"
+        venue="Fluno Center"
+        location="Madison" 
+        img={data.cardImage3}
+        />
+        <p></p>
         <ContentCard 
         startDate="Mar. 31" 
         title="Higher Education Cybersecurity: UW–Madison Is Moving FORWARD!"
@@ -115,7 +162,7 @@ return (
         venue="Capitol Lakes Retirement Community"
         location="Madison" 
         />
-        <p>Story card (S), no image</p>
+        <p></p>
         <ContentCard 
         title="Coachella Valley"
         category="UW NOW"
@@ -123,9 +170,9 @@ return (
         url="#"
         tags={taglist2}
         />
-
+<br />
 <h2>1/2 (M) Content Card</h2>
-<p>This size card displays at 256px wide on mobile and 529px wide on all larger screens.</p>
+<p>This size card displays at 256px wide on mobile and 528px wide on all larger screens.</p>
 
         <ContentCard 
         title="Coachella Valley"
@@ -187,7 +234,7 @@ return (
         size="L"
         />
         <p>Images are required for (L) size cards.</p>
-        <p>In the example above, the title is cut off at two lines and the content fits within the maximum height for the card. In the example below, the title is longer and the Card layout begins to break down.</p>      
+        <p>In the example above, the title is cut off at two lines and the content fits within the maximum height for the card. In the example below, the title is longer and the Card layout begins to break down (at desktop).</p>      
         <ContentCard 
         startDate="Apr. 3" 
         title="The Past, Present, and Future of Rainstorms and Floods in Wisconsin and around the World"
@@ -197,6 +244,30 @@ return (
         img={data.cardImage3}
         size="L"
         />
+        <br />
+        <p>When we use a very long title on a story, the layout begins to break down.</p>
+        <ContentCard 
+        title="The Past, Present, and Future of Rainstorms and Floods in Wisconsin and around the World"
+        category="UW NOW"
+        excerpt="First played in 1902, the Rose Bowl is the nation’s oldest college football bowl game. This season, the Badgers will make their 10th trip to Pasadena for the big game and their first trip back since 2013.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere" 
+        url="#"
+        tags={taglist2}
+        img={data.cardImage7}
+        size="L"
+
+        />
+        <p>The long title works okay when there is no excerpt</p>
+        <ContentCard 
+        title="The Past, Present, and Future of Rainstorms and Floods in Wisconsin and around the World"
+        category="UW NOW"
+        excerpt="First played in 1902" 
+        url="#"
+        tags={taglist2}
+        img={data.cardImage7}
+        size="L"
+
+        />
+        
         <hr>
         
         </hr>
@@ -276,6 +347,24 @@ query {
       }
     }
     cardImage5: file(relativePath: { eq: "lead_720_405-2-1-a@2x.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 712,  quality: 100) {
+          ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluidLimitPresentationSize
+
+        }
+      }
+    }
+    cardImage6: file(relativePath: { eq: "fball_ILL_band18_0556-3@2x.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 712,  quality: 100) {
+          ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluidLimitPresentationSize
+
+        }
+      }
+    }
+    cardImage7: file(relativePath: { eq: "19AP_DowntownMadisonInc_NewFacesNPlaces_Manis_13@2x.png" }) {
       childImageSharp {
         fluid(maxWidth: 712,  quality: 100) {
           ...GatsbyImageSharpFluid
