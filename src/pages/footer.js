479b751d6aaf8c5b861e45662e16413c8e1c345b
Anne Fonicello <anne.fonicello@supportuw.org>
2020-07-22 14:24:53 -0500

    Update footer page notes

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -11,10 +11,8 @@ export default () => {
         <p>The footer appears at the bottom of all pages:</p>
 
         <p>At mobile (screens smaller than 768px wide), the footer format changes to a single column.</p>
-        <p>Social Media icons will be added in a future sprint along with the dark red diagonal that crosses through them.</p>
-        <p>Currently in mobile view, the order of the UW Site Links and the general navigation links are flipped. This will be fixed in a future sprint.</p>     
-        <p>Crest image needs to be replaced with higher quality version</p>
-        <p>Current spacing between bottom of the navigation lists and the bottom border in desktop is slightly off. This is due to a padding issue that will be fixed in a future sprint</p>
+        <p>Social Media icons currently do not change color when hovered over, this will be fixed in a future release</p>
+        <p>Some consideration should be done for how we want the footer to appear when between the larger tablet sizes and the smaller 375 mobile size.</p>
     </PageSection>
 </Layout>
     )
