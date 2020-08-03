9a4de0e3e2679e351b7ec7209fb5bfbe8136f18b
James Rotering <james.rotering@supportuw.org>
2020-08-03 14:39:09 -0500

    update yarn packages, troubleshoot the primary menu

--- components/parts/PrimaryMenu.js
+++ components/parts/PrimaryMenu.js
@@ -21,11 +21,13 @@ class Submenu extends React.Component {
     )
   }
  
-  close() {
+  close(num) {
+    console.log('submenu close(', num, ') ')
     this.setState({ open: false })
   }
 
   open() {
+    console.log('submenu open() ')
   
     this.setState({ open: true })
   }
@@ -37,6 +39,10 @@ class Menu extends React.Component {
     this.state = {
       open: false,
       openSub: false,
+      open1: false,
+      open2: false,
+      open3: false,
+      open4: false,
     }
   }
 
@@ -51,18 +57,16 @@ class Menu extends React.Component {
             
           <ul className="primary">
             <li 
-              onClick={() => this.openSubMenuClick(1)} 
-              onMouseEnter={() => this.openSubMenu(1)}
-              onMouseLeave={() => this.closeSubMenu(1)}
+              onClick={() => this.toggleSubMenu(1)} 
+              onHover={() => this.openSubMenuHover(1)}
+              className={ this.state.open1 ? 'open' : ''}
             >
               <span>Alumni Communities</span>
               <Submenu 
                 onMouseEnter={() => this.openSubMenu(1)} 
                 ref={el => (this.childMenu1 = el)} 
               >
-                <li 
-                  onClick={() => this.closeSubMenuClick(1)} 
-                  className={`returnNav ${this.state.openSub ? "opensub" : ""}`}><span>Alumni Communities</span></li>
+                
                 <li><Link to="/#eenie">1Eenie</Link></li>
                 <li><Link to="/#meenie">2Meenie</Link></li>
                 <li><Link to="/#meinie">3Meinie</Link></li>
@@ -70,20 +74,15 @@ class Menu extends React.Component {
               </Submenu>
               </li>
             <li 
-              onClick={() => this.openSubMenuClick(2)} 
-              onMouseEnter={() => this.openSubMenu(2)} 
-              onMouseLeave={() => this.closeSubMenu(2)} 
+              onClick={() => this.toggleSubMenu(2)} 
+              onHover={() => this.openSubMenu(2)} 
+              className={ this.state.open2 ? 'open' : ''}
             >
               <span>Events and Activities</span>
               <Submenu 
                 onMouseEnter={() => this.openSubMenu(2)} 
                 ref={el => (this.childMenu2 = el)}
               >
-                <li 
-                  onClick={() => this.closeSubMenuClick(2)} 
-                  className={`returnNav ${this.state.openSub ? "opensub" : ""}`}>
-                  <span>Events and Activities</span>
-                </li>
                 <li><Link to="/#eenie">5Eenie</Link></li>
                 <li><Link to="/#meenie">6Meenie</Link></li>
                 <li><Link to="/#meinie">7Meinie</Link></li>
@@ -91,21 +90,15 @@ class Menu extends React.Component {
               </Submenu>
               </li>
             <li 
-              onClick={() => this.openSubMenuClick(3)}  
-              onMouseEnter={() => this.openSubMenu(3)} 
-              onMouseLeave={() => this.closeSubMenu(3)}
+              onClick={() => this.toggleSubMenu(3)}  
+              onHover={() => this.openSubMenu(3)} 
+              className={ this.state.open3 ? 'open' : ''}
             >
               <span>Stories</span>
               <Submenu 
                 ref={el => (this.childMenu3 = el)}
                 onMouseEnter={() => this.openSubMenu(3)} 
               >
-                <li 
-                  className={`returnNav ${this.state.openSub ? "opensub" : ""}`}
-                  onClick={() => this.closeSubMenuClick(3)}
-                >
-                  <span>Stories</span>
-                </li>
                 <li><Link to="/#eenie">9Eenie</Link></li>
                 <li><Link to="/#meenie">10Meenie</Link></li>
                 <li><Link to="/#meinie">11Meinie</Link></li>
@@ -113,21 +106,15 @@ class Menu extends React.Component {
               </Submenu>
             </li>
             <li 
-              onClick={() => this.openSubMenuClick(4)}  
-              onMouseEnter={() => this.openSubMenu(4)} 
-              onMouseLeave={() => this.closeSubMenu(4)}
+              onClick={() => this.toggleSubMenu(4)}  
+              onHover={() => this.openSubMenu(4)} 
+              className={ this.state.open4 ? 'open' : ''}
             >
               <span>Ways to Support</span>
               <Submenu 
                 ref={el => (this.childMenu4 = el)}
                 onMouseEnter={() => this.openSubMenu(4)} 
               >
-                <li 
-                  className={`returnNav ${this.state.openSub ? "opensub" : ""}`}
-                  onClick={() => this.closeSubMenuClick(4)} 
-                >
-                  <span>Ways to Support</span>
-                </li>
                 <li><Link to="/#eenie">13Eenie</Link></li>
                 <li><Link to="/#meenie">14Meenie</Link></li>
                 <li><Link to="/#meinie">15Meinie</Link></li>
@@ -184,65 +171,122 @@ class Menu extends React.Component {
     this.setState({ open: true })
   }
   openSubMenu(num) {
+    console.log('openSubMenu:',num)
     switch(num) {
       case 1: 
       this.childMenu1.open()
-      this.childMenu2.close()
-      this.childMenu3.close()
-      this.childMenu4.close()
+      this.childMenu2.close(2)
+      this.childMenu3.close(3)
+      this.childMenu4.close(4)
+      this.setState({ open1: true })
+      
       break
       case 2: 
       this.childMenu2.open()
-      this.childMenu1.close()
-      this.childMenu3.close()
-      this.childMenu4.close()
+      this.childMenu1.close(1)
+      this.childMenu3.close(3)
+      this.childMenu4.close(4)
+      
+      this.setState({ open2: true })
+     
       break
       case 3: 
       this.childMenu3.open()
-      this.childMenu1.close()
-      this.childMenu2.close()
-      this.childMenu4.close()
+      this.childMenu1.close(1)
+      this.childMenu2.close(2)
+      this.childMenu4.close(4)
+      this.setState({ open1: false })
+      this.setState({ open2: false })
+      this.setState({ open3: true })
+      this.setState({ open4: false })
       break
       case 4: 
       this.childMenu4.open()
-      this.childMenu1.close()
-      this.childMenu2.close()
-      this.childMenu3.close()
+      this.childMenu1.close(1)
+      this.childMenu2.close(2)
+      this.childMenu3.close(3)
+      this.setState({ open1: false })
+      this.setState({ open2: false })
+      this.setState({ open3: false })
+      this.setState({ open4: true })
       break
       default: 
       this.childMenu1.open()
-      this.childMenu2.close()
-      this.childMenu3.close()
-      this.childMenu4.close()
+      this.childMenu2.close(2)
+      this.childMenu3.close(3)
+      this.childMenu4.close(4)
       break
     }
-  //this.setState({ openSub: true })
+  this.setState({ openSub: true })
     
   }
-  openSubMenuClick(num) {
+  openSubMenuHover(num) {
+    console.log('this was a hover trigger')
     this.openSubMenu(num)
-    this.setState({ openSub: true })
   }
+  
   closeSubMenu(num) {
+    console.log('closeSubMenu:',num)
     switch(num) {
       case 1: this.childMenu1.close()
+      this.setState({ open1: false })
       break
       case 2: this.childMenu2.close()
+      this.setState({ open2: false })
       break
       case 3: this.childMenu3.close()
+      this.setState({ open3: false })
       break
       case 4: this.childMenu4.close()
+      this.setState({ open4: false })
       break
       default: this.childMenu1.close()
       break
     }
-   // this.setState({ openSub: false })
+  
     
   }
-  closeSubMenuClick(num) {
-    this.setState({ openSub: false })
-    this.closeSubMenu(num)
-    console.log('closeSubmenuClick')
+  
+ 
+  toggleSubMenu(num) {
+    console.log('toggleSubMenu:',num)
+    switch(num) {
+      case 1: 
+      console.log('is menu1 open? ', this.childMenu1.state.open )
+        this.childMenu1.state.open 
+        ? 
+          this.closeSubMenu(1)
+        : this.openSubMenu(1)
+
+      break
+      case 2: 
+      console.log('is menu2 open? ', this.childMenu1.state.open )
+      this.childMenu2.state.open 
+        ? this.closeSubMenu(2)
+        : this.openSubMenu(2)
+      break
+      case 3: 
+      console.log('is menu3 open? ', this.childMenu1.state.open )
+      this.childMenu3.state.open 
+      ? this.closeSubMenu(3)
+      : this.openSubMenu(3)
+      break
+      case 4: 
+      console.log('is menu4 open? ', this.childMenu1.state.open )
+      this.childMenu4.state.open 
+      ? this.closeSubMenu(4)
+      : this.openSubMenu(4)
+      break
+      default: 
+      this.childMenu1.close()
+      this.childMenu2.close()
+      this.childMenu3.close()
+      this.childMenu4.close()
+      break
+    }
+    this.setState({ openSub: !this.state.openSub })
+
+
   }
 }
 
@@ -255,6 +299,9 @@ export default React.forwardRef((props, ref) => {
     },
     toggle() {
       menuRef.current.toggle()
+    },
+    toggleSubMenu() {
+      menuRef.current.toggleSubMenu()
     }
   }))
 
