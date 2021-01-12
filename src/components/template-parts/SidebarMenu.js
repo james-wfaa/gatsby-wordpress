7fec84f6a2af4bc176d3ba59f06e3283e37463b2
Sarah Loach <sarah.loach@supportuw.org>
2021-01-12 13:14:47 -0600

    Only show arrow toggle smaller than laptopS

--- src/components/template-parts/SidebarMenu.js
+++ src/components/template-parts/SidebarMenu.js
@@ -53,23 +53,26 @@ const SidebarMenu = ({name, menuItems, width}) => {
       margin-left: 32px;
       padding: 0;
     }
-    h4::after {
-      content:'';
-      border: solid #c5050c;
-      border-width: 0 3px 3px 0;
-      display: inline-block;
-      padding: 5px;
-      transform: rotate(45deg);
-      -webkit-transform: rotate(45deg);
-      margin-left:20px;
-      vertical-align: top;
-      margin-top: 4px;
+    @media screen and ${breakpoints.laptopSMax}{
+      h4::after {
+        content:'';
+        border: solid #c5050c;
+        border-width: 0 3px 3px 0;
+        display: inline-block;
+        padding: 5px;
+        transform: rotate(45deg);
+        -webkit-transform: rotate(45deg);
+        margin-left:20px;
+        vertical-align: top;
+        margin-top: 4px;
+      }
+      h4.open::after{
+        transform: rotate(-135deg);
+        -webkit-transform: rotate(-135deg);
+        vertical-align: middle;
+      }
     }
-    h4.open::after{
-      transform: rotate(-135deg);
-      -webkit-transform: rotate(-135deg);
-      vertical-align: middle;
-    }
+    
   `
   const ModalHandler = styled.div`
     cursor: pointer;
