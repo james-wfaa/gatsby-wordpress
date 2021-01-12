c75c4edf377d8070595dae7abc9dca76e7882373
James Rotering <james.rotering@supportuw.org>
2021-01-12 10:45:17 -0600

    code cleanup

--- src/components/template-parts/wordpress-post.js
+++ src/components/template-parts/wordpress-post.js
@@ -1,15 +1,13 @@
 import React from "react"
 
-import { Link } from "gatsby"
 import Layout from "../layout"
-import { normalizePath } from "../../utils/get-url-path"
 import WordPressContent from "../content-blocks/WordPressPostContent"
 import TitleSection from '../parts/WordPressTitleSection'
 import SocialShareLinks from '../parts/SocialShareLinks'
 import FeaturedImage from "../content-blocks/FeaturedImage"
 
 function BlogPost({ data }) {
-  const { nextPage, previousPage, page } = data
+  const { page } = data
   const { title, content, featuredImage, categories, author, date, excerpt, link } = page
 
   return (
@@ -19,7 +17,6 @@ function BlogPost({ data }) {
             <FeaturedImage featuredImage={featuredImage} />
         )}
         <WordPressContent content={content} />
-      <br />
       <SocialShareLinks className="SocailShare" text="Share This Story" title={title} excerpt={excerpt} url={link}/>
     </Layout>
   )
