e89ee233523b903b3bb8f784e17ea08c6e149d13
Sarah Loach <sarah.loach@supportuw.org>
2021-01-15 10:49:22 -0600

    First pass at logic for grabbing correct image based on width

--- src/components/template-parts/wordpress-post.js
+++ src/components/template-parts/wordpress-post.js
@@ -11,11 +11,34 @@ function BlogPost({ data }) {
   const { page } = data
   const { title, content, featuredImage, categories, author, date, excerpt, heroImage, link } = page
   console.log(heroImage, featuredImage)
-  const image = heroImage && heroImage?.heroImage?.localFile?.childImageSharp 
+  //if featuredImage is at least 1080px
+  //otherwise heroImage from old site if it's at least 718px (render at 712)
+  //else featured image from the old site (render at natural width)
+  let heroSize = heroImage.heroImage && heroImage.heroImage.mediaDetails.width ? heroImage.heroImage.mediaDetails.width : null
+  let featSize = featuredImage?.node?.mediaDetails.width ? featuredImage?.node?.mediaDetails.width : null
+  let size = featSize > heroSize ? featSize : heroSize
+  console.log(size)
+  
+  let image = null
+  if ((size >= 1080) && featuredImage?.node?.localFile?.childImageSharp.fluid){
+    image = featuredImage?.node
+    console.log('using 1080, wfeat')
+  } else if ((size >= 718) && heroImage?.heroImage?.localFile?.childImageSharp){
+    image = heroImage.heroImage
+    console.log('using 718 hero')
+  } else if ((size >= 718) && featuredImage?.node?.localFile?.childImageSharp){
+    image = featuredImage.node
+    console.log('using 718 feat', featuredImage?.node?.localFile?.childImageSharp)
+  } else if(size < 718){
+    image = featuredImage.node
+    console.log('using less than 718 feat')
+  }
+  //old image logic
+  /*const image = heroImage && heroImage?.heroImage?.localFile?.childImageSharp 
     ? heroImage.heroImage 
     : featuredImage?.node
       ? featuredImage.node
-      : null
+      : null*/
   //console.log(page)
   return (
     <Layout>
