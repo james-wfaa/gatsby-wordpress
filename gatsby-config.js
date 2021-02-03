83322a0e786a8afe8db2bc64b1787049906c4afe
James Rotering <james.rotering@supportuw.org>
2021-02-03 16:00:19 -0600

    load all forms but #16

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,8 +90,8 @@ module.exports = {
       options: {
           // Base URL needs to include protocol (http/https)
           baseUrl: 'https://uwalumni.wpengine.com',
-          include: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], // Array of form IDs. Will only import these forms.
-          //exclude: [], // Array of form IDs. Will exclude these forms.
+          //include: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], // Array of form IDs. Will only import these forms.
+          exclude: [16], // Array of form IDs. Will exclude these forms.
           // Gravity Forms API
           allowSelfSigned: true,
           api: {
