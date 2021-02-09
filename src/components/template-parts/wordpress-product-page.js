ac8a2109d5ca411aa10eb7451d46c84e3476174b
James Rotering <james.rotering@supportuw.org>
2021-02-09 15:09:36 -0600

    bit of syntax cleanup

--- src/components/template-parts/wordpress-product-page.js
+++ src/components/template-parts/wordpress-product-page.js
@@ -14,32 +14,28 @@ function WordPressPage({ page }) {
   /* extract the events to pass along with the blocks as helper data */
   const { eventCategory } = eventListing
 
-  const navContents = (wpChildren.nodes[0]) ? wpChildren.nodes.map((node) => {
-    console.log("Nav: " +  node.uri);
-    return node;
-  }
-  ) : ''
+  const navContents = (wpChildren.nodes[0]) 
+    ? wpChildren.nodes.map((node) => {
+      console.log("Nav: " +  node.uri);
+      return node
+    }) 
+    : ''
 
-  const normalizedButtons = (buttons) ? buttons.map(item=>{
-    let buttonLink = "";
-    if(item.goToEvents){
-      buttonLink = "#event-listing";
-    }
-    else{
-      if(item.buttonLink){
-        buttonLink = item.buttonLink.uri;
+  const normalizedButtons = (buttons) 
+    ? buttons.map(item=>{     
+      let buttonLink = (item.goToEvents) 
+        ? "#event-listing"
+        : (item.buttonLink?.uri)
+          ? item.buttonLink.uri
+          : (item.buttonExternalLinkUrl) 
+            ? item.buttonExternalLinkUrl
+            : '#'
+      return {
+        link: buttonLink,
+        text: item.buttonText
       }
-      else if(item.buttonExternalLinkUrl){
-        buttonLink = item.buttonExternalLinkUrl
-      }
-    }
-    return {
-      link: buttonLink,
-      text: item.buttonText
-    }
-
-  }
-  ) : ''
+    }) 
+  : ''
   return (
     <Layout title={title}>
       { featuredImage?.node && (
