addaabcb69c1ccb759235c718552ea9e4be188db
Shane Fisher <shane@ordermygear.com>
2021-02-11 23:08:45 -0600

    sorting for sidebar menu items

--- src/components/template-parts/wordpress-page.js
+++ src/components/template-parts/wordpress-page.js
@@ -37,7 +37,7 @@ function WordPressPage({ page }) {
     `
   )
   */
- //menuName = (ancestors) ? 
+ //menuName = (ancestors) ?
 
  //console.log(ancestors)
  if (ancestors?.nodes) {
@@ -53,21 +53,28 @@ function WordPressPage({ page }) {
       menuRoot = ancestors.nodes[ancestors.nodes.length -2]
     }
    }
-   
+
  }
 
  if (menuRoot) {
-  //console.log(menuRoot)
-
-  wpMenu = {}
-  wpMenu.name = menuRoot.title
-  wpMenu.link = menuRoot.link
-  wpMenu.menuItems = {}
-  wpMenu.menuItems.nodes = menuRoot.wpChildren.nodes.map( item => {
-   item.path = item.uri
-   item.label = item.title
-   return item
- })
+   wpMenu = {}
+   wpMenu.name = menuRoot.title
+   wpMenu.link = menuRoot.link
+   wpMenu.menuItems = {}
+   wpMenu.menuItems.nodes = menuRoot.wpChildren.nodes.map(item => {
+     item.path = item.uri
+     item.label = item.title
+     return item
+   })
+   wpMenu.menuItems.nodes.sort((a,  b) => {
+     if (a.title < b.title) {
+       return -1
+     }
+     if (a.title > b.title) {
+       return 1
+     }
+     return 0
+   })
  }
 
 
@@ -92,7 +99,7 @@ function WordPressPage({ page }) {
         { wpMenu && (
           <Menu name={wpMenu.name} link={wpMenu.link} menuItems={wpMenu.menuItems.nodes} width={width} />
         )}
-          
+
           <PageSection heading={title} pageTitle leftAlign defaultPage divider>
             <WordPressContent blocks={blocks}  content={content} />
           </PageSection>
