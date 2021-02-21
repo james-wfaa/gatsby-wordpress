68b16b466b65b0ad35195e89ccb95f8ef9bd33ae
Shane Fisher <shane@ordermygear.com>
2021-02-20 22:40:40 -0600

    additional working embeds

--- src/components/content-blocks/WordPressBasicContentBlocks.js
+++ src/components/content-blocks/WordPressBasicContentBlocks.js
@@ -64,7 +64,7 @@ function handleError({values, error, reset}) {
 function handleSuccess({values, reset, confirmations}) {
     //handle success
 }
-    
+
 
 const WordPressContentBlocks = ({className, blocks, content, eventCategory, stagger}) => {
 
@@ -72,54 +72,121 @@ const WordPressContentBlocks = ({className, blocks, content, eventCategory, stag
 
         const RenderedBlocks = (blocks) ? blocks.map((block) => {
         const borderTop = (block.originalContent.indexOf(' border-top') > 0)
-        switch(block.name) {
-            case "core/separator":
-                return(<div dangerouslySetInnerHTML={{__html: block.originalContent}} />)
-                break
-            case "core/group":
-            case "acf/events-listing-section":
-                break
-            case "acf/accordion-navigation":
-                return <AccordionNavigation className={block.name.replace('/', '-')} block={block} />
-                break
-            case "core/columns":
-                return (<Column className={block.name.replace('/', '-')} block={block} />)
-            case "core/buttons":
-                if(block.innerBlocks && block.innerBlocks[0].originalContent){
-                    let innerRenderedBlocks = [];
-                    block.innerBlocks.forEach((innerBlock) => {
-                        innerRenderedBlocks.push(<Block className={innerBlock.name.replace('/', '-')} block={innerBlock} />)
-                    })
-                    return (<div className={block.name.replace('/', '-')}>{innerRenderedBlocks}</div>)
-                }
-                break
-                case "gravityforms/form":
-                    const shortcode = ((block.isDynamic) ? block.dynamicContent : block.originalContent)
-                    let idStart = shortcode.indexOf('id="')
-                    if (idStart > -1) {
-                        idStart += 4
-                        let idEnd = shortcode.indexOf('"', idStart)
-                        //console.log(idEnd)
-                        //console.log(idStart)
-                        const formId = shortcode.substring(idStart,idEnd)
-                        //(formId)
-                        return (<GravityForm className={block.name.replace('/', '-')} id={formId} />)
-                    }
-                    
-                    break
-    
-            //Add case to handle news/stories that use the freeform block but do not have blocks... and then use content instead of original content because it has the html tags
-            //Also added css below that is duplicated from WPBlock
-            case "core/freeform":
-                return(<div className={block.name.replace('/', '-')} dangerouslySetInnerHTML={{__html: content}} />)
-                break
-            case "core-embed/flickr":
-                return <EmbedBlock data={block.originalContent} />
-                break
-            
-            default:
-                return (<Block className={block.name.replace('/', '-')} block={block} />)
-                break
+        switch (block.name) {
+          case "core/separator":
+            return (
+              <div
+                dangerouslySetInnerHTML={{ __html: block.originalContent }}
+              />
+            )
+            break
+          case "core/group":
+          case "acf/events-listing-section":
+            break
+          case "acf/accordion-navigation":
+            return (
+              <AccordionNavigation
+                className={block.name.replace("/", "-")}
+                block={block}
+              />
+            )
+            break
+          case "core/columns":
+            return (
+              <Column className={block.name.replace("/", "-")} block={block} />
+            )
+          case "core/buttons":
+            if (block.innerBlocks && block.innerBlocks[0].originalContent) {
+              let innerRenderedBlocks = []
+              block.innerBlocks.forEach(innerBlock => {
+                innerRenderedBlocks.push(
+                  <Block
+                    className={innerBlock.name.replace("/", "-")}
+                    block={innerBlock}
+                  />
+                )
+              })
+              return (
+                <div className={block.name.replace("/", "-")}>
+                  {innerRenderedBlocks}
+                </div>
+              )
+            }
+            break
+          case "gravityforms/form":
+            const shortcode = block.isDynamic
+              ? block.dynamicContent
+              : block.originalContent
+            let idStart = shortcode.indexOf('id="')
+            if (idStart > -1) {
+              idStart += 4
+              let idEnd = shortcode.indexOf('"', idStart)
+              //console.log(idEnd)
+              //console.log(idStart)
+              const formId = shortcode.substring(idStart, idEnd)
+              //(formId)
+              return (
+                <GravityForm
+                  className={block.name.replace("/", "-")}
+                  id={formId}
+                />
+              )
+            }
+
+            break
+
+          //Add case to handle news/stories that use the freeform block but do not have blocks... and then use content instead of original content because it has the html tags
+          //Also added css below that is duplicated from WPBlock
+          case "core/freeform":
+            return (
+              <div
+                className={block.name.replace("/", "-")}
+                dangerouslySetInnerHTML={{ __html: content }}
+              />
+            )
+            break
+          case "core-embed/flickr":
+            return <EmbedBlock data={block.originalContent} />
+            break
+          case "core-embed/vimeo":
+            return (
+              <div className="wp-block-embed">
+                <EmbedBlock source={block.originalContent} type="vimeo" />
+              </div>
+            )
+            break
+          case "core-embed/youtube":
+            return (
+              <div className="wp-block-embed">
+                <EmbedBlock source={block.originalContent} type="youtube" />
+              </div>
+            )
+            break
+          case "core-embed/instagram":
+            return (
+              <div className="wp-block-embed">
+                <EmbedBlock source={block.originalContent} type="instagram" />
+              </div>
+            )
+            break
+          case "core/embed":
+            return (
+              <div className="wp-block-embed">
+                <EmbedBlock source={block.originalContent} type="base" />
+              </div>
+            )
+          default:
+            if (block.originalContent.length > 0) {
+                return (
+                  <Block
+                    className={block.name.replace("/", "-")}
+                    block={block}
+                  />
+                )
+              } else {
+                return null
+              }
+            break
         }
     }
     ) : null
@@ -128,7 +195,7 @@ const WordPressContentBlocks = ({className, blocks, content, eventCategory, stag
     return(
         <div className={className} id="Top">
             { RenderedBlocks && (
-                
+
                 <div className="content">
                     {RenderedBlocks}
                 </div>
