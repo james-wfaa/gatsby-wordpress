6f1fac998cf5d55164b6e37215a88c67da725e2c
Sarah Loach <sarah.loach@supportuw.org>
2021-02-24 11:06:27 -0600

    Add mobile styles and setup class/prop to pass for no underline option

--- src/components/page-sections/BreadCrumbs.js
+++ src/components/page-sections/BreadCrumbs.js
@@ -1,35 +1,45 @@
 import React from "react"
 import styled from "styled-components"
-import { colors, sizes } from "../css-variables"
+import { colors, sizes, breakpoints } from "../css-variables"
 
-const BreadCrumbs = ({ links }) => {
+const BreadCrumbs = ({ links, nounderline }) => {
   const BreadCrumbSection = styled.div`
-    display: flex;
+    display: none;
+    width: 90%;
     max-width: 1080px;
     margin: 0 auto;
+    @media screen and ${breakpoints.tablet} {
+      display: flex;
+    }
   `
   const BreadCrumb = styled.div`
     display: flex;
-    margin: 5px 0 5px 5px;
+    margin: 12px 0 5px 5px;
     color: ${colors.copyText};
     font-size: ${sizes.s14};
+    @media screen and ${breakpoints.laptopS} {
+      margin: 12px 0 5px 5px;
+    }
     a {
-      text-decoration: underline;
       margin-right: 5px;
       color: ${colors.copyText};
       cursor: pointer;
+      &.noUnderline{
+        text-decoration: none;
+      }
     }
     p {
       margin-bottom: 0;
     }
   `
+  let underline = nounderline ? 'noUnderline' : null;
   let lastItem = links[links.length - 1]
-  
+
   let renderedLinks = links.map(link => {
     const notLast = link !== lastItem ? true : false
     return (
       <BreadCrumb>
-        <a href={link.url}>
+        <a href={link.url} className={underline}>
           <p>{link.name}</p>
         </a>
         {notLast && <p>{`>`}</p>}
