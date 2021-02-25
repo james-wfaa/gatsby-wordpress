b8a76c47a1e13566b95b078f8a699d3066b68db8
Anne Fonicello <anne.fonicello@supportuw.org>
2021-02-25 15:54:45 -0600

    uwa-1459 and uwa-1460 Add External Link to notes, and add a "More Alumni Notes" section

---
+++ src/components/page-sections/RecentNotes.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
+import ContentCard from "../content-blocks/ContentCard"
+import SimpleSlider from "../content-modules/SimpleSlider"
+
+const RecentNotes = () => {
+  const { allWpClassnote } = useStaticQuery(
+    graphql`
+      query {
+        allWpClassnote(limit: 6, sort: {order: DESC, fields: date}) {
+          nodes {
+            title
+            excerpt
+            featuredImage {
+              node {
+                localFile {
+                  ...HeroImage
+                }
+              }
+            }
+            url: uri
+          }
+        }
+      }
+    `
+  )
+
+  let postCards = allWpClassnote.nodes.map((post) => {
+    const { featuredImage: img } = post
+    const cardImg = (img && img.node && img.node.localFile) ? img.node.localFile : null
+    return (
+      <ContentCard key={post.url} img={cardImg} {...post} />
+    )
+  })
+
+  return (
+    <div>
+       <SimpleSlider
+          className="center"
+          slidesToShow="1"
+          dots
+          centerMode
+          variableWidth>
+            {postCards}
+          </SimpleSlider>
+    </div>
+  )
+}
+
+export default RecentNotes
