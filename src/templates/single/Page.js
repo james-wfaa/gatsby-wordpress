9585cee54e6ffedad4a26566f6f27f0bb81a75f4
James Rotering <james.rotering@supportuw.org>
2021-03-08 16:20:57 -0600

    Get sponsors on page

--- src/templates/single/Page.js
+++ src/templates/single/Page.js
@@ -300,6 +300,21 @@ export const query = graphql`
       chapterLevel {
         chapterLevel
       }
+      chapterSponsors {
+        sponsors {
+          sponsorName
+          sponsorText
+          sponsorLogo {
+            localFile {
+              childImageSharp {
+                fixed {
+                  aspectRatio
+                }
+              }
+            }
+          }
+        }
+      }
     }
     allWp {
       nodes {
