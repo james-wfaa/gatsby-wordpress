ac9437457bf3f2e0220271ad1200f4113a12858f
Shane Fisher <shane@ordermygear.com>
2021-03-14 19:50:23 -0500

    working filters

---
+++ src/pages/chapter-search.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ChapterSearch from "../components/parts/ChapterSearch/ChapterSearch"
+import Layout from "../components/layout"
+
+const ChapterSearchPage = () => {
+  return (
+    <div>
+      <Layout>
+        <ChapterSearch />
+      </Layout>
+    </div>
+  )
+}
+
+export default ChapterSearchPage
