11290f2930abe3ba22fdd94037be8889a10fed31
Anne Fonicello <anne.fonicello@supportuw.org>
2021-03-26 13:30:57 -0500

    uwa-1590 Footer Updates

--- src/components/parts/HeaderSocialIcons.js
+++ src/components/parts/HeaderSocialIcons.js
@@ -3,11 +3,11 @@ const HeaderSocialIcons = ({ classList }) => {
 
     return ( 
         <ul className="socialLinks">
-            <li><a className="fb" title="Follow WAA on Facebook" href="https://www.facebook.com/wisalumni"></a></li>
-            <li><a className="tw" title="Follow WAA on Facebook Twitter" href="https://www.twitter.com/wisalumni"></a></li>
-            <li><a className="ig" title="Follow WAA on Facebook Instagram" href="https://www.instagram.com/wisalumni"></a></li>
-            <li><a className="wc" title="Follow WAA on Facebook WeChat" href="/wechat"></a></li>
-            <li><a className="li" title="Follow WAA on Facebook LinkedIn" href="https://www.linkedin.com/company/wisconsin-alumni-association"></a></li>
+            <li><a className="fb" title="Follow WAA on Facebook" href="https://www.facebook.com/wisalumni" target="_blank"></a></li>
+            <li><a className="tw" title="Follow WAA on Twitter" href="https://www.twitter.com/wisalumni" target="_blank"></a></li>
+            <li><a className="ig" title="Follow WAA on Instagram" href="https://www.instagram.com/wisalumni" target="_blank"></a></li>
+            <li><a className="wc" title="Follow WAA on WeChat" href="/wechat" target="_blank"></a></li>
+            <li><a className="li" title="Follow WAA on LinkedIn" href="https://www.linkedin.com/company/wisconsin-alumni-association" target="_blank"></a></li>
         </ul>
     )
 }
