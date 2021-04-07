e02ebe6f283df0e471ba0a9bf32df6b7e514feab
Anne Fonicello <anne.fonicello@supportuw.org>
2021-04-07 15:17:47 -0500

    uwa-1650 Fix html and url in Email and Facebook share buttons

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    siteURL: 'https://gatsbyuwalumni.gtsb.io',
   },
   plugins: [
     `gatsby-plugin-sharp`,
