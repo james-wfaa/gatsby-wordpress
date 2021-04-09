e062c7755b3e2e4acac03bf749970371ed415408
Anne Fonicello <anne.fonicello@supportuw.org>
2021-04-09 11:10:55 -0500

    uwa-557 404 Page Template

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,41 +1,50 @@
 import React from 'react'
 import Layout from '../components/layout';
 import styled from 'styled-components'
-
+import {sizes, colors, fonts, breakpoints} from '../components/css-variables'
 
 const Page404 = () => {
-    const styled404 = styled.div `
+
+    const OuterWrapper = styled.div`
         margin:100px auto 50px;
-        max-width:1080px;
-        min-height:400px
+        max-width: 300px;
+        min-height:400px;
+        @media screen and ${breakpoints.tabletS} {
+            max-width: 534px;
+        }
+        @media screen and ${breakpoints.laptopS} {
+            max-width:1080px;
+        }
         h1 {
-            color: rgb(197, 5, 12);
-            font-family: mrs-eaves-xl-serif, serif;
+            color: ${colors.titleColor};
+            font-family: ${fonts.eaves};
             font-weight: bold;
             font-style: italic;
-            margin-bottom:75px;
+            margin-bottom: ${sizes.s72};
         }
-        div h1:after {
+        h1:after {
             display: block;
             margin: 30px auto;
             position: absolute;
             left: calc(0 - 30px);
             height: 0.444rem;
             width: calc(3.778rem);
-            background-color: rgb(197, 5, 12);
+            background-color: ${colors.titleColor};
             content: "";
+            
         }
-        div p a {
-            color:rgb(197, 5, 12);
+        p a {
+            color:${colors.titleColor};
         }
+    
     `
     return(
         <Layout>
-            <styled404>
+            <OuterWrapper>
                 <h1>Oops, Something Went Wrong</h1>
                 <p>The page you were looking for was not found. Maybe the page has moved. Or, the address is outdated. Was the URL typed correctly? Perhaps the site is on break and went to get some Babcock ice cream.</p>
                 <p>Whatever the reason for this unfortunate error, please use the menu or <a href="/search">site search</a> to get where you want to go. You can reach out at any time using our chat feature or you can check out our <a href="/customer-service-faq/">frequently asked questions</a>.</p>
-            </styled404>
+            </OuterWrapper>
         </Layout>
     )
 }
