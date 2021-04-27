ffc9c1f7e8742a8b701abeac9dc36edd7881db7e
James Rotering <james.rotering@supportuw.org>
2021-04-27 09:17:56 -0500

    update header social icons

--- src/components/parts/HeaderSocialIcons.js
+++ src/components/parts/HeaderSocialIcons.js
@@ -3,11 +3,11 @@ const HeaderSocialIcons = ({ classList }) => {
 
     return ( 
         <ul className="socialLinks">
-            <li><a className="fb" title="Follow WAA on Facebook" href="https://www.facebook.com/wisalumni">Follow WAA on Facebook</a></li>
-            <li><a className="tw" title="Follow WAA on Twitter" href="https://www.twitter.com/wisalumni">Follow WAA on Facebook Twitter</a></li>
-            <li><a className="ig" title="Follow WAA on Instagram" href="https://www.instagram.com/wisalumni">Follow WAA on Facebook Instagram</a></li>
-            <li><a className="wc" title="Follow WAA on WeChat" href="/about/wechat">Follow WAA on Facebook WeChat</a></li>
-            <li><a className="li" title="Follow WAA on LinkedIn" href="https://www.linkedin.com/company/wisconsin-alumni-association">Follow WAA on Facebook LinkedIn</a></li>
+            <li><a className="fb" target="_blank" title="Follow WAA on Facebook" href="https://www.facebook.com/wisalumni">Follow WAA on Facebook</a></li>
+            <li><a className="tw" target="_blank" title="Follow WAA on Twitter" href="https://www.twitter.com/wisalumni">Follow WAA on Twitter</a></li>
+            <li><a className="ig" target="_blank" title="Follow WAA on Instagram" href="https://www.instagram.com/wisalumni">Follow WAA on Instagram</a></li>
+            <li><a className="wc" target="_blank" title="Learn more About following WAA on WeChat" href="/about/wechat">Learn more About following WAA on WeChat</a></li>
+            <li><a className="li" target="_blank" title="Follow WAA on LinkedIn" href="https://www.linkedin.com/company/wisconsin-alumni-association">Follow WAA on LinkedIn</a></li>
         </ul>
     )
 }
