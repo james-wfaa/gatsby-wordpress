f20d71d9b218f6af6a8f1579be77afcfa8c2e76a
Shane Fisher <shane@ordermygear.com>
2021-05-02 02:39:21 -0500

    moves script add to useeffect

--- src/components/content-blocks/AdvocacyEmbed.js
+++ src/components/content-blocks/AdvocacyEmbed.js
@@ -2,33 +2,32 @@ import React, { useEffect, useState } from 'react'
 import parse from 'html-react-parser';
 import Helmet from 'react-helmet'
 
+
+
 const AdvocacyEmbed = ({ block }) => {
   const parsed = (block?.dynamicContent) ? parse(block.dynamicContent) : null
   const advocacyId = (parsed?.props?.id) ? (parsed.props.id) : null
 
-  // useEffect(() => {
-  //     const loaded = document.getElementById("p2a-campaign-container");
-  //     console.log(loaded)
-  //     if (!loaded) {
-  //       const script = document.createElement("script");
-  //       script.id = 'advocacy-actionwidget-code';
-  //       script.src = '//p2a.co/js/embed/widget/advocacywidget.min.js';
-  //       script.async = false;
-  //       document.body.appendChild(script);
-  //     }
-  // }, [])
+  const addScript = url => {
+    const script = document.createElement("script")
+    script.src = url
+    script.async = true
+    document.body.appendChild(script)
+  }
+
+  useEffect(() => {
+    if (!document.getElementById('advocacy-actionwidget-code')) {
+      addScript("https://p2a.co/js/embed/widget/advocacywidget.min.js")
+    }
+  }, [])
 
   return (
-<<<<<<< HEAD
-  <div className="advocacy-embed">
-    <Helmet>
-      <script src="https:////p2a.co/js/embed/widget/advocacywidget.min.js"></script>
-    </Helmet>
-=======
-  <div className="embed-block advocacy-embed">
->>>>>>> 38269cdb06ae2f68f97fb4a5a7289c5a28c537b0
-    <div className="advocacy-actionwidget" data-domain="p2a.co" data-shorturl={advocacyId}></div>
-  </div>)
+    <div>
+      <div className="embed-block advocacy-embed">
+        <div className="advocacy-actionwidget" data-domain="p2a.co" data-shorturl={advocacyId}></div>
+      </div>
+    </div>
+  )
 }
 
 
