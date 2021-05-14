58695e0215edb2668bf2317b1069dccac0cd0f73
James Rotering <james.rotering@supportuw.org>
2021-05-14 10:26:26 -0500

    breathe in, breathe out

--- gatsby-config.js
+++ gatsby-config.js
@@ -284,7 +284,7 @@ module.exports = {
         },
         serialize: ({
           site,
-          //allSitePage,
+          allSitePage,
           allWpEvent,
           allWpClassnote,
           allWpPost,
