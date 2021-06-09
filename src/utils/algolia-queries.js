5df282087642f483865e7459e69ef55cd47d106a
Anne Fonicello <anne.fonicello@supportuw.org>
2021-06-09 15:31:42 -0500

    Update algolia date parsing

--- src/utils/algolia-queries.js
+++ src/utils/algolia-queries.js
@@ -1,5 +1,4 @@
 
-
 const eventQuery = `{
   events: allWpEvent {
     edges {
@@ -276,13 +275,13 @@ function eventToAlgoliaRecord({ node: { id, blocks, date, endDate, startDate, ev
   let startDateTimestamp = new Date(startDate).getTime() / 1000
   let endDateTimestamp = new Date(endDate).getTime() / 1000
   let isTrip = eventDetails.trip
-  let formattedStartDate = shortDate(startDate)
-  let formattedEndDate = shortDate(endDate)
+  let formattedStartDate = startDate ? shortDate(startDate) : null
+  let formattedEndDate = endDate ? shortDate(endDate) : null
   let formattedLongDate = null
   let options = { year: 'numeric', month: 'long', day: 'numeric' };
-  let parsedStartDate = new Date(startDate).toLocaleDateString('en-US', options)
-  let parsedEndDate = new Date(endDate).toLocaleDateString('en-US', options)
-  let parsedTime = convertTime(startDate, endDate)
+  let parsedStartDate = startDate ? new Date(startDate).toLocaleDateString('en-US', options) : null
+  let parsedEndDate = endDate ? new Date(endDate).toLocaleDateString('en-US', options) : null
+  let parsedTime = startDate ? convertTime(startDate, endDate) : null
 
 
   if (blocks) {
@@ -405,6 +404,64 @@ function chapterToAlgoliaRecord({node: { id, date, link, chapterDetails, ...rest
     date: dateTimestamp,
     ...rest,
   }
+}
+
+function convertTime ({node: { istartTime, endTime}}) {
+  const startDS = new Date(startTime.replace(/\s/, 'T'));
+  const endDS = new Date(endTime.replace(/\s/, 'T'));
+  if(startDS.getDate() === endDS.getDate()){
+    const startTime = formatAMPM(startDS);
+    const endTime = formatAMPM(endDS);
+    let strTime = '';
+    if(startTime[0].ampm === endTime[0].ampm){
+      strTime = startTime[0].time  + '&ndash;' + endTime[0].time + ' ' + endTime[0].ampm;
+    }
+    else {
+      strTime = startTime[0].time + ' ' + startTime[0].ampm + '&ndash;' + endTime[0].time + ' ' + endTime[0].ampm;
+    }
+    return strTime;
+
+  }
+  else{
+
+  }
+}
+function formatAMPM ({node: {date}}) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  const ampm = hours >= 12 ? 'p.m.' : 'a.m.';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+
+  let strTime = hours
+  if(minutes && minutes !== '00'){
+    strTime += ':' + minutes;
+  }
+  const timeObj = [{
+    time: strTime,
+    ampm: ampm,
+  }]
+  return timeObj;
+}
+
+function shortDate ({node: {date}}) {
+  if (!date) {
+    return null
+  }
+  let tmpDate
+  if (typeof date !== 'string') {
+    tmpDate = new Date(date)
+  } else {
+    tmpDate = new Date(date.replace(/\s/, 'T'))
+  }
+
+  const monthNames = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "June",
+  "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."
+];
+  const month = monthNames[tmpDate.getMonth()]
+  const dd = tmpDate.getDate()
+  return `${month} ${dd}`
 }
 
 const queries = [
